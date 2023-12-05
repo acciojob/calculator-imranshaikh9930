4db0eabcc8145d48eb5cfc7c36f1d76a91c4f537
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-12-05 15:18:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', function () {
   function handleEquals() {
     try {
         currentInput = currentInput.replace(/[^-()\d/*+.]/g, '');
-        currentInput = parseFloat(eval(currentInput).toFixed(10)).toString();
+        currentInput = parseInt(eval(currentInput).toFixed(10)).toString();
     } catch (error) {
         currentInput = 'Error';
     }
